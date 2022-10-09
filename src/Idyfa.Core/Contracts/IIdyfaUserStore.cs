836210d4e4aac6cd3c8070d8ed146aab8c78cd11
imun <imun22@gmail.com>
2022-10-09 11:50:37 +0330

    Add IIdifyUserStore contract

---
+++ src/Idyfa.Core/Contracts/IIdyfaUserStore.cs
@@ -0,0 +1,62 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace Idyfa.Core.Contracts;
+
+public interface IIdyfaUserStore
+{
+    
+    Task<IdentityResult> CreateAsync(
+        User user, CancellationToken cancellationToken = default);
+    
+    Task<IdentityResult> UpdateAsync(
+        User user, CancellationToken cancellationToken = default);
+
+    Task<IdentityResult> DeleteAsync(
+        User user, CancellationToken cancellationToken = default);
+    
+    Task<User> FindByIdAsync(
+        string userId, CancellationToken cancellationToken = default);
+    
+    Task<User> FindByUserNameAsync(
+        string userName, CancellationToken cancellationToken = default);
+    
+    Task<UserRole> FindUserRoleAsync(
+        string userId, string roleId, CancellationToken cancellationToken);
+    
+    Task<UserLogin> FindUserLoginAsync(
+        string userId, string loginProvider, string providerKey, CancellationToken cancellationToken);
+    
+    Task AddToRoleAsync(
+        User user, string normalizedRoleName, CancellationToken cancellationToken = default);
+    
+    Task RemoveFromRoleAsync(
+        User user, string normalizedRoleName, CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyCollection<string>> GetRolesAsync(User user, CancellationToken cancellationToken = default);
+    
+    Task<bool> IsInRoleAsync(
+        User user, string normalizedRoleName, CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyCollection<Claim>> GetClaimsAsync(
+        User user, CancellationToken cancellationToken = default);
+    
+    Task AddClaimsAsync(
+        User user, IEnumerable<Claim> claims, CancellationToken cancellationToken = default);
+    
+    Task ReplaceClaimAsync(
+        User user, Claim claim, Claim newClaim, CancellationToken cancellationToken = default);
+    
+    Task RemoveClaimsAsync(
+        User user, IEnumerable<Claim> claims, CancellationToken cancellationToken = default);
+    
+    Task<User> FindByEmailAsync(
+        string email, CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyCollection<User>> GetUsersInRoleAsync(
+        string roleName, CancellationToken cancellationToken = default);
+
+    Task AddUserTokenAsync(UserToken token);
+    
+    Task RemoveUserTokenAsync(UserToken token);
+}
