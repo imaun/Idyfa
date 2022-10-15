521ff896a40c9c54d4a83d7e7df4720746f392dd
imun <imun22@gmail.com>
2022-10-15 17:25:40 +0330

    Add EntityConfigurations for EntityFramework project

---
+++ src/Idyfa.EntityFrameworkCore/Configurations/EntityConfigurations.cs
@@ -0,0 +1,31 @@
+using Idyfa.Core;
+
+namespace Idyfa.EntityFrameworkCore.Configurations;
+
+public static partial class EntityConfigurations
+{
+    private static Dictionary<Type, string> _tableNames = new Dictionary<Type, string>()
+    {
+        { typeof(User), nameof(User) },
+        { typeof(Role), nameof(Role) },
+        { typeof(UserCategory), nameof(UserCategory) },
+        { typeof(UserClaim), nameof(UserClaim) },
+        { typeof(UserLogin), nameof(UserLogin) },
+        { typeof(UserRole), nameof(UserRole) },
+        { typeof(RoleClaim), nameof(RoleClaim) },
+        { typeof(UserLoginRecord), nameof(UserLoginRecord) },
+        { typeof(UserToken), nameof(UserToken) },
+        { typeof(UserUsedPassword), nameof(UserUsedPassword) },
+        { typeof(PermissionRecord), nameof(PermissionRecord) }
+    };
+
+    private static string GetTableName(Type t, string tableNamePrefix = "")
+    {
+        if (!_tableNames.ContainsKey(t))
+        {
+            throw new Exception($"The type '{t.FullName}' is not a valid Idyfa Entity type.");
+        }
+
+        return $"{tableNamePrefix}{_tableNames[t]}";
+    }
+}
---
+++ src/Idyfa.EntityFrameworkCore/Configurations/UserConfig.cs
@@ -0,0 +1,17 @@
+using Idyfa.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Idyfa.EntityFrameworkCore.Configurations;
+
+public static partial class EntityConfigurations
+{
+
+    /// <summary>
+    /// Configure <see cref="User"/> mapping for the Database Schema.
+    /// </summary>
+    /// <param name="builder"></param>
+    public static void AddUserConfiguration(this ModelBuilder builder)
+    {
+        
+    }
+}
