cdc97125c9763677d5984ea8ef616e5f7da503ac
imun <imun22@gmail.com>
2022-10-18 12:28:07 +0330

    Add ApiKey & ReferralCode fields to User entity

--- src/Idyfa.Core/Models/User.cs
+++ src/Idyfa.Core/Models/User.cs
@@ -11,16 +11,36 @@ public class User : IdentityUser<string>
     #region props
 
     public string FirstName { get; protected set; }
+    
     public string LastName { get; protected set; }
+    
     public string NationalCode { get; protected set; }
+    
     public string DisplayName { get; set; }
+    
     public UserStatus Status { get; protected set; }
+    
     public DateTime? BirthDate { get; protected set; }
+    
     public DateTime CreateDate { get; protected set; }
+    
     public DateTime RegisterDate { get; protected set; }
+    
     public DateTime? ModifyDate { get; protected set; }
+    
     public DateTime? LastStatusChanged { get; protected set; }
+    
     public DateTime? LastVisitDate { get; protected set; }
+    
+    /// <summary>
+    /// ApiKey for authenticating the user via a random string.
+    /// </summary>
+    public string ApiKey { get; protected set; }
+    
+    /// <summary>
+    /// The code that <see cref="User"/>s can use for referring their friends. 
+    /// </summary>
+    public string ReferralCode { get; protected set; }
     
     public Guid? CategoryId { get; protected set; }
     #endregion
