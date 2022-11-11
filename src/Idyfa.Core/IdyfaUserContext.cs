acf9679c93a5f54442c2e91bcf167732cd5c8ec3
imaun <imun22@gmail.com>
2022-11-11 21:16:37 +0330

    Fix UserContext implementation

--- src/Idyfa.Core/IdyfaUserContext.cs
+++ src/Idyfa.Core/IdyfaUserContext.cs
@@ -1,10 +1,5 @@
 ﻿using Idyfa.Core.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace Idyfa.Core
@@ -39,5 +34,6 @@ namespace Idyfa.Core
         public bool? PhoneNumberConfirmed => Principal?.Identity?.GetUserPhoneNumberConfirmed() ?? null;
 
         public string? Email => Principal?.Identity?.GetUserEmail() ?? null;
+
     }
 }
