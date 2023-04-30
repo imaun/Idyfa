3304939cc8b515bb369c1d42c34b2b64c527ad26
imuan <imun22@gmail.com>
2023-04-30 12:16:16 +0330

    add-migration UserTwoFactorProvider for SQLite

--- src/Idyfa.EntityFrameworkCore.SQLite/Migrations/20230430084516_UserTwoFactorProvider.Designer.cs
+++ src/Idyfa.EntityFrameworkCore.SQLite/Migrations/20230430084516_UserTwoFactorProvider.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Idyfa.EntityFrameworkCore.SQLite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Idyfa.EntityFrameworkCore.SQLite.Migrations
 {
     [DbContext(typeof(IdyfaSQLiteDbContext))]
-    partial class IdyfaSQLiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230430084516_UserTwoFactorProvider")]
+    partial class UserTwoFactorProvider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
---
+++ src/Idyfa.EntityFrameworkCore.SQLite/Migrations/20230430084516_UserTwoFactorProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Idyfa.EntityFrameworkCore.SQLite.Migrations
+{
+    public partial class UserTwoFactorProvider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastTwoFactorCode",
+                table: "Idyfa.User",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorProvider",
+                table: "Idyfa.User",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorProvider",
+                table: "Idyfa.User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastTwoFactorCode",
+                table: "Idyfa.User",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
