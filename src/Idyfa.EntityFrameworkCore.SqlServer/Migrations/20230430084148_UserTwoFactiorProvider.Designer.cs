cff98612f31a82c6d2e92d9e3a9052ba4f7c26b8
imuan <imun22@gmail.com>
2023-04-30 12:14:29 +0330

    add-migration UserTwoFactorProvider for SQL Server

--- src/Idyfa.EntityFrameworkCore.SqlServer/Migrations/20230430084148_UserTwoFactiorProvider.Designer.cs
+++ src/Idyfa.EntityFrameworkCore.SqlServer/Migrations/20230430084148_UserTwoFactiorProvider.Designer.cs
@@ -4,6 +4,7 @@ using Idyfa.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Idyfa.EntityFrameworkCore.SqlServer.Migrations
 {
     [DbContext(typeof(IdyfaSqlServerDbContext))]
-    partial class IdyfaSqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230430084148_UserTwoFactiorProvider")]
+    partial class UserTwoFactiorProvider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Idyfa.EntityFrameworkCore.SqlServer/Migrations/20230430084148_UserTwoFactiorProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Idyfa.EntityFrameworkCore.SqlServer.Migrations
+{
+    public partial class UserTwoFactiorProvider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorProvider",
+                table: "Idyfa.User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorProvider",
+                table: "Idyfa.User");
+        }
+    }
+}
